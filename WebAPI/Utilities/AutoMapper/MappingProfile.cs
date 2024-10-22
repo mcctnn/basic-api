34e5c285707ffbc83c664c961d677dfb17138b1c
Mehmet Can Çetin <92088210+mcctnn@users.noreply.github.com>
2024-10-22 19:29:43 +0300

    Add files via upload

---
+++ WebAPI/Utilities/AutoMapper/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Entities.DataTransferObjects;
+using Entities.Model;
+
+namespace WebAPI.Utilities.AutoMapper
+{
+    public class MappingProfile:Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<BookDtoForUpdate, Book>().ReverseMap();
+            CreateMap<Book, BookDto>();
+            CreateMap<BookDtoForInsertion, Book>();
+            CreateMap<UserForRegistrationDto, User>();
+            CreateMap<CategoryDtoForUpdate, Category>().ReverseMap();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<CategoryDtoForInsertion,Category>();
+        }
+    }
+}
